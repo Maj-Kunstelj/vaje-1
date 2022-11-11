29efd441aac84e5dd954923d0bdf8353e7830c49
Maj Kunstelj <117371148+Maj-Kunstelj@users.noreply.github.com>
2022-11-11 08:50:52 +0100

    Update Naloga3.java

--- Zanke/Naloga3.java
+++ Zanke/Naloga3.java
@@ -18,3 +18,58 @@ class Naloga3 {
 
     }
 }
+
+import java.util.Scanner;
+
+class Naloga2 {
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Vpisite 1. stevilo: ");
+        double a1 = scan.nextDouble();
+
+        Scanner scan9 = new Scanner(System.in);
+        System.out.print("Vpisite 2. stevilo: ");
+        double a2 = scan9.nextDouble();
+
+        Scanner scan8 = new Scanner(System.in);
+        System.out.print("Vpisite 3. stevilo: ");
+        double a3 = scan8.nextDouble();
+
+        Scanner scan7 = new Scanner(System.in);
+        System.out.print("Vpisite 4. stevilo: ");
+        double a4 = scan7.nextDouble();
+
+        Scanner scan6 = new Scanner(System.in);
+        System.out.print("Vpisite 5. stevilo: ");
+        double a5 = scan6.nextDouble();
+
+        Scanner scan5 = new Scanner(System.in);
+        System.out.print("Vpisite 6. stevilo: ");
+        double a6 = scan5.nextDouble();
+        
+        Scanner scan4 = new Scanner(System.in);
+        System.out.print("Vpisite 7. stevilo: ");
+        double a7 = scan4.nextDouble();
+        
+        Scanner scan3 = new Scanner(System.in);
+        System.out.print("Vpisite 8. stevilo: ");
+        double a8 = scan3.nextDouble();
+
+        Scanner scan2 = new Scanner(System.in);
+        System.out.print("Vpisite 9. stevilo: ");
+        double a9 = scan2.nextDouble();
+
+        Scanner scan1 = new Scanner(System.in);
+        System.out.print("Vpisite 10. stevilo: ");
+        double a10 = scan1.nextDouble();
+
+        double f = a1 * a2 * a3 * a4 * a5 * a6 * a7 * a8 * a9 * a10;
+        double k = a1 + a2 + a3 + a4 + a5 + a6 + a7 + a8 + a9 + a10;
+        double i = k / 10;
+
+        System.out.println("--------------");
+        System.out.println("Zmnozek stevil: " + f);
+        System.out.println("Povprecje stevil: " + i);
+    }
+}
